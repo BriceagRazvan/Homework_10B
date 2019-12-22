baebd441b56734a097cf719e908b25243bf7a714
BriceagRazvan <59146455+BriceagRazvan@users.noreply.github.com>
2019-12-22 20:37:49 +0200

    Add files via upload

---
+++ Brad.java
@@ -0,0 +1,20 @@
+
+public class BRADLAINFORMATICA {
+public static void main(String[]args) {
+    char a='\u20dd';
+    char b='\u203e';
+    System.out.printf("%9c%n",'*' );
+    System.out.printf("%8c%2c%n",'/','\\');
+    System.out.printf("%7c%2c%2c%n",'/',a,'\\');
+    System.out.printf("%6c%2c%2c%2c%n",'/',a,a,'\\');
+    System.out.printf("%5c%2c%2c%2c%2c%n",'/',a,a,a,'\\');
+    System.out.printf("%4c%2c%2c%2c%2c%2c%n",'/',a,a,a,a,'\\');
+    System.out.printf("%3c%2c%2c%2c%2c%2c%2c%n",'/',a,a,a,a,a,'\\');
+    System.out.printf("%2c%2c%2c%2c%2c%2c%2c%2c%n",'/',a,a,a,a,a,a,'\\');
+    System.out.printf("%c%2c%2c%2c%2c%2c%2c%2c%2c%n",'/',a,a,a,a,a,a,a,'\\');
+    System.out.printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%n",b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b);
+    System.out.printf("%8c%c%c%n",'|','|','|');
+    System.out.printf("%8c%c%c%n",'|','|','|');
+    System.out.printf("%8c%c%c%n",'|','|','|');
+    }
+}
